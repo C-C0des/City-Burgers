8624a569742df3b916116a4669c5f19f37bf7786
Diaspar <the.diaspar@gmail.com>
2018-12-14 16:30:41 -0500

    test

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 // Node Dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -7,11 +8,11 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 //Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
-// app.use(express.static('public'));
+//app.use(express.static(__dirname + '/public'));
+ app.use(express.static('public'));
 
 // Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true}));
 
 // Handlebars
 var exphbs = require('express-handlebars');
